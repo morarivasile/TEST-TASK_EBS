5b4d9183bff7560461caf1521a7b83e737e337e6
Vasile Morari <vasile.morari@icloud.com>
2020-10-28 18:01:17 +0200

    Added Single App Template

---
+++ EBS_TEST/EBS_TEST/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EBS_TEST
+//
+//  Created by Vasile Morari on 28.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
