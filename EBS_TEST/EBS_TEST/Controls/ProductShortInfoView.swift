92bf92ed236e7f948e296d5f20ab92bc8190164a
Vasile Morari <vasile.morari@icloud.com>
2020-10-30 08:43:59 +0200

    Created ProductShortInfoView

---
+++ EBS_TEST/EBS_TEST/Controls/ProductShortInfoView.swift
@@ -0,0 +1,113 @@
+//
+//  ProductShortInfoView.swift
+//  EBS_TEST
+//
+//  Created by Vasile Morari on 30.10.2020.
+//
+
+import UIKit
+
+final class ProductShortInfoView: UIView {
+    
+    // MARK: - IBOutlets
+    
+    private var titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.systemFont(ofSize: 24, weight: .bold)
+        label.textAlignment = .center
+        label.textColor = .titleBlue
+        return label
+    }()
+    
+    private var subtitleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.systemFont(ofSize: 20, weight: .regular)
+        label.textAlignment = .center
+        label.textColor = .black
+        return label
+    }()
+    
+    private var priceLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupLayout()
+    }
+    
+    func setTitle(_ title: String?) {
+        titleLabel.text = title
+    }
+    
+    func setSubtitle(_ subtitle: String?) {
+        subtitleLabel.text = subtitle
+    }
+    
+    func setPriceLabel(price: String, oldPrice: String?) {
+        priceLabel.attributedText = getAttributedPrice(from: price, oldPrice: oldPrice)
+    }
+    
+    // MARK: - Private
+    
+    private func setupLayout() {
+        addSubview(titleLabel)
+        addSubview(subtitleLabel)
+        addSubview(priceLabel)
+        
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: topAnchor, constant: 16),
+            titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
+            titleLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16),
+            
+            subtitleLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 8),
+            subtitleLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
+            subtitleLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16),
+            
+            priceLabel.topAnchor.constraint(equalTo: subtitleLabel.bottomAnchor, constant: 8),
+            priceLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
+            priceLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16),
+            priceLabel.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -16)
+        ])
+    }
+    
+    private func getAttributedPrice(from price: String, oldPrice: String?) -> NSAttributedString {
+        if let oldPrice = oldPrice {
+            
+            let priceString = getMainAttributedPrice("\(price) - ")
+            let oldPriceString = getSecondartAttributedPrice(oldPrice)
+            
+            priceString.append(oldPriceString)
+            
+            return priceString
+        } else {
+            return getMainAttributedPrice(price)
+        }
+    }
+    
+    private func getMainAttributedPrice(_ price: String) -> NSMutableAttributedString {
+        return NSMutableAttributedString(string: price, attributes: [
+            .font: UIFont.systemFont(ofSize: 24, weight: .regular),
+            .foregroundColor: UIColor.priceBlue
+        ])
+    }
+    
+    private func getSecondartAttributedPrice(_ price: String) -> NSAttributedString {
+        return NSAttributedString(string: price, attributes: [
+            .font: UIFont.systemFont(ofSize: 20, weight: .regular),
+            .foregroundColor: UIColor.oldPriceGray,
+            NSAttributedString.Key.strikethroughStyle: 1
+        ])
+    }
+}
+
--- EBS_TEST/EBS_TEST/Screens/Products List/Cells/ProductListTableViewCell/ProductListTableViewCell.swift
+++ EBS_TEST/EBS_TEST/Screens/Products List/Cells/ProductListTableViewCell/ProductListTableViewCell.swift
@@ -12,11 +12,8 @@ final class ProductListTableViewCell: UITableViewCell {
     
     @IBOutlet weak private var productImageView: UIImageView!
     
-    @IBOutlet weak private var titleLabel: UILabel!
+    @IBOutlet weak private var shortInfoView: ProductShortInfoView!
     
-    @IBOutlet weak private var subtitleLabel: UILabel!
-    
-    @IBOutlet weak private var priceLabel: UILabel!
     
     override func prepareForReuse() {
         super.prepareForReuse()
@@ -24,38 +21,12 @@ final class ProductListTableViewCell: UITableViewCell {
     }
     
     func configure(for viewModel: ProductViewModel) {
-        titleLabel.text = viewModel.title
-        subtitleLabel.text = viewModel.description
-        priceLabel.attributedText = getAttributedPrice(from: viewModel.displayPrice)
+        shortInfoView.setTitle(viewModel.title)
+        shortInfoView.setSubtitle(viewModel.description)
+        shortInfoView.setPriceLabel(price: viewModel.displayPrice.price, oldPrice: viewModel.displayPrice.oldPrice)
+        
         productImageView.setImage(stringURL: viewModel.imageStringURL)
     }
     
-    private func getAttributedPrice(from model: ProductViewModel.DisplayPrice) -> NSAttributedString {
-        if let oldPrice = model.oldPrice {
-            
-            let priceString = getMainAttributedPrice("\(model.price) - ")
-            let oldPriceString = getSecondartAttributedPrice(oldPrice)
-            
-            priceString.append(oldPriceString)
-            
-            return priceString
-        } else {
-            return getMainAttributedPrice(model.price)
-        }
-    }
     
-    private func getMainAttributedPrice(_ price: String) -> NSMutableAttributedString {
-        return NSMutableAttributedString(string: price, attributes: [
-            .font: UIFont.systemFont(ofSize: 24, weight: .regular),
-            .foregroundColor: UIColor.priceBlue
-        ])
-    }
-    
-    private func getSecondartAttributedPrice(_ price: String) -> NSAttributedString {
-        return NSAttributedString(string: price, attributes: [
-            .font: UIFont.systemFont(ofSize: 20, weight: .regular),
-            .foregroundColor: UIColor.oldPriceGray,
-            NSAttributedString.Key.strikethroughStyle: 1
-        ])
-    }
 }
